2142f4a9096b584314ecf1ed8a919223d2a9c829
Hugo Vila <code.hvila@gmail.com>
2022-09-08 14:15:20 +0200

    Create a list of events

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [events, setEvents] = useState([
+    { title: "My birthday has passed", id: 1 },
+    { title: "The traffic light turned red", id: 2 },
+    { title: "The supermarket just opened", id: 3 },
+    { title: "This game is over", id: 4 },
+    { title: "A key has been pressed to play again", id: 5 },
+  ]);
+
+  const handleClick = (id) => {
+    setEvents((prevEvents) => {
+      return prevEvents.filter((prevEvent) => {
+        return id !== prevEvent.id;
+      });
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {events.map((event) => (
+        <div key={event.id}>
+          <p>{event.title}</p>
+          <button onClick={() => handleClick(event.id)}>delete</button>
+        </div>
+      ))}
     </div>
   );
 }
